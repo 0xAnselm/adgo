44bf67a88d39ab6ec988ceb7987662ee1a5b3108
anselm <anselm.amwege@gmail.com>
2023-09-14 13:30:35 +0200

    added: channels

--- channels/main.go
+++ channels/main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	fName := "links.txt"
-	links := getLinks(fName)
+	// fName := "links.txt"
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(3 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
 }
 
-func getLinks() []string {
-	file, err := io.Open(fName)
+func checkLink(l string, c chan string) {
+	resp, err := http.Get(l)
+	if err != nil {
+		log.Fatal(err)
+		c <- l
+		os.Exit(1)
+		return
+	}
+	fmt.Println("Link:", resp.Status, l)
+	c <- l
+}
+
+func getLinks(fName string) []string {
+	file, err := os.Open(fName)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	links := make(links []string, 32*1024)
-	io.Copy(links, file)
-	return links
+
+	fc := make([]byte, 32*1024)
+	file.Read(fc)
+	fcs := strings.Split(string(fc), "\n")
+	return fcs
 }
