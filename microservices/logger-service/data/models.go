2e0d10c75ba7c444100ef26384752917b472bd05
anselm <anselm.amwege@gmail.com>
2023-10-11 14:21:33 +0200

    logger done

--- microservices/logger-service/data/models.go
+++ microservices/logger-service/data/models.go
@@ -22,7 +22,7 @@ type LogEntry struct {
 	Name      string    `bson:"name" json:"name"`
 	Data      string    `bson:"data" json:"data"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
 
 func New(mongo *mongo.Client) Models {
